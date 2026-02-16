aa1bfe7357abced9eec392b5f20b88b2c228ee13
SL1YYY <jacobtheman62@gmail.com>
2026-02-15 23:13:59 -0500

    Update init.lua

--- modules/init.lua
+++ modules/init.lua
@@ -1,29 +1,17 @@
 -- This file is licensed under the Creative Commons Attribution 4.0 International License. See https://creativecommons.org/licenses/by/4.0/legalcode.txt for details.
-
--- Initialize controller locks FIRST
-if not getgenv().controller then
-	getgenv().controller = {}
-end
-
-if not getgenv().controller.lock then
-	getgenv().controller.lock = {
-		knife = false,
-		general = false
-	}
-end
-
 local player = game:GetService("Players").LocalPlayer
 
 function init()
-	local playerModel = workspace:FindFirstChild(player.Name)
+	local playerModel = workspace:WaitForChild(player.Name)
 	if playerModel then
-		local gun = playerModel:FindFirstChild("GunController")
+		local gun = playerModel:WaitForChild("GunController")
 		if gun then
 			pcall(function()
 				gun:Destroy()
 			end)
 		end
-		local knife = playerModel:FindFirstChild("KnifeController")
+
+		local knife = playerModel:WaitForChild("KnifeController")
 		if knife then
 			pcall(function()
 				knife:Destroy()
@@ -32,13 +20,5 @@ function init()
 	end
 end
 
--- Run immediately
-init()
-
--- Also run on respawn
-player.CharacterAdded:Connect(function()
-	task.wait(0.5)
-	init()
-end)
-
-return true
+task.spawn(init)
+return player.CharacterAdded:Connect(init)
