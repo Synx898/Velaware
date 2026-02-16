55b2f4768c565ee17954b73c6fcc9f8c89b7aba3
SL1YYY <jacobtheman62@gmail.com>
2026-02-15 23:46:29 -0500

    Update init.lua

--- modules/init.lua
+++ modules/init.lua
@@ -1,24 +1,43 @@
 -- This file is licensed under the Creative Commons Attribution 4.0 International License. See https://creativecommons.org/licenses/by/4.0/legalcode.txt for details.
+
+-- Initialize controller locks
+if not getgenv().controller then
+	getgenv().controller = {}
+end
+
+if not getgenv().controller.lock then
+	getgenv().controller.lock = {
+		knife = false,
+		general = false
+	}
+end
+
 local player = game:GetService("Players").LocalPlayer
 
 function init()
-	local playerModel = workspace:WaitForChild(player.Name)
+	local playerModel = workspace:FindFirstChild(player.Name)
 	if playerModel then
-		local gun = playerModel:WaitForChild("GunController")
+		-- ONLY destroy gun controller
+		local gun = playerModel:FindFirstChild("GunController")
 		if gun then
 			pcall(function()
 				gun:Destroy()
 			end)
+			print("[Velaware] Gun controller destroyed")
 		end
-
-		local knife = playerModel:WaitForChild("KnifeController")
-		if knife then
-			pcall(function()
-				knife:Destroy()
-			end)
-		end
+		
+		-- Leave knife controller alone - don't destroy it!
+		-- Game's knife system will work normally
 	end
 end
 
-task.spawn(init)
-return player.CharacterAdded:Connect(init)
+-- Run immediately
+init()
+
+-- Also run on respawn
+player.CharacterAdded:Connect(function()
+	task.wait(0.5)
+	init()
+end)
+
+return true
